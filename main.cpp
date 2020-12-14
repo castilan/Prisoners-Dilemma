c497a82d0aedb371e571d9344d4e2b79db268ce6
castilan <53244903+castilan@users.noreply.github.com>
2020-12-14 12:39:54 -0800

    format adjustments

--- main.cpp
+++ main.cpp
@@ -17,20 +17,23 @@ int main() {
 
 	// Ask the user how many rounds
 	// If they give an invalid input or input is <= 0, exit
-	cout << "How many rounds do you want to play?\n";
+	cout << "How many turns do you want to play?\n";
 	cin >> num_rounds;
 	cin.get();
+
 	if (num_rounds <= 0) {
 		return 0;
 	}
 
 	// Loop over all combinations of player strategies and print game report
 	for (int p1_strategy=0; p1_strategy < NUM_STRATEGIES; p1_strategy++) {
-		for (int p2_strategy=0; p2_strategy < NUM_STRATEGIES; 
+
+	    for (int p2_strategy=0; p2_strategy < NUM_STRATEGIES;
 			p2_strategy++) {
 			cout << endl;
 			g.Reset(possible_strategies[p1_strategy],
 				possible_strategies[p2_strategy]);
+
 			for (int i=1; i <= num_rounds; i++) {
 				g.PlayTurn();
 				g.TurnReport(cout, i);
